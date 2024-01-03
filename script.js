c291eb36d59a42fda1f1e53cf9165bd45f4185a0
Nero <83870907+qNerowo@users.noreply.github.com>
2024-01-03 20:47:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.banner-slide');
 
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Iniciar com o primeiro slide visível
+showSlide(currentSlide);
